fffa1741b487c2ad622f2e7e5e53b0c217d2cefe
niraj1234 <niraj.webtech@gmail.com>
2024-09-28 00:48:39 +0530

    removing some css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,11 +5,11 @@ const Welcome = () => {
   return (
     <div>
         <h2>Welcome to React Firebase Application</h2>
-    <div style={{backgroundColor:'#BB7788', padding:'20px',borderBottom:'3px solid #DD8888',borderRadius:'7px' , margin:'20px'}}>
-      <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/login'>Login</Link>
-      <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/signup'>Signup</Link>
-      <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/dashboard/studentList'>Student List</Link>
-      <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/dashboard/facultyList'>Faculty List</Link>
+    <div style={{backgroundColor:'#BB7788', padding:'20px',borderBottom:'3px solid #DD8888', margin:'10px'}}>
+      <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/login'>Login</Link>
+      <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/signup'>Signup</Link>
+      <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/dashboard/studentList'>Student List</Link>
+      <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/dashboard/facultyList'>Faculty List</Link>
     </div>
     </div>
   )
