79f10cb820457f1cf219da5b8975db1149d524c2
niraj1234 <niraj.webtech@gmail.com>
2024-09-28 00:50:15 +0530

    removing some css 2

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
   return (
     <div>
         <h2>Welcome to React Firebase Application</h2>
-    <div style={{backgroundColor:'#BB7788', padding:'20px',borderBottom:'3px solid #DD8888', margin:'10px'}}>
+    <div style={{backgroundColor:'#FDFDFD', padding:'20px',borderBottom:'3px solid #DD8888', margin:'10px'}}>
       <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/login'>Login</Link>
       <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/signup'>Signup</Link>
       <Link style={{ fontSize:'18px', padding:'5px' , margin:'10px'}} to='/dashboard/studentList'>Student List</Link>
