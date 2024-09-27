00bce5bfba17c24eb513ed2ee9787f01d43493f4
niraj1234 <niraj.webtech@gmail.com>
2024-09-28 00:42:22 +0530

    Adding welcome page Redmenu bar

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
   return (
     <div>
         <h2>Welcome to React Firebase Application</h2>
-    <div style={{backgroundColor:'#44AAAA', padding:'20px',borderBottom:'3px solid #DD8888',borderRadius:'7px' , margin:'20px'}}>
+    <div style={{backgroundColor:'#BB7788', padding:'20px',borderBottom:'3px solid #DD8888',borderRadius:'7px' , margin:'20px'}}>
       <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/login'>Login</Link>
       <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/signup'>Signup</Link>
       <Link style={{backgroundColor:'#fdfdfd', borderRadius:'3px', fontWeight:'bold', textDecoration:'none',fontSize:'18px', padding:'5px' , margin:'10px'}} to='/dashboard/studentList'>Student List</Link>
